278fcaefd288f43e3c9fa7bd702c0f364ee3fbb7
Kewne <67972089+kewne7768@users.noreply.github.com>
2024-06-30 06:25:02 +0200

    Make import/export buttons look nicer

--- evolve_automation.user.js
+++ evolve_automation.user.js
@@ -15358,14 +15358,17 @@ declare global {
     }
 
     function buildImportExport() {
-        let importExportNode = $(".importExport").last();
-        if (importExportNode === null) {
+        let importExportBase = $(".importExport").last();
+        if (importExportBase === null) {
             return;
         }
 
-        if (document.getElementById("script_settingsImport") !== null) {
+        if (document.getElementById("script_importExportButtons") !== null) {
             return;
         }
+
+        let importExportNode = $('<div id="script_importExportButtons" style="margin-top: 6px">');
+        importExportBase.after(importExportNode);
 
         importExportNode.append(' <button id="script_settingsImport" class="button">Import Script Settings</button>');
 

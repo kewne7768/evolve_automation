4d6fc4f0d783ea19693b848c7d2085902fa11e44
Kewne <67972089+kewne7768@users.noreply.github.com>
2024-11-29 06:38:22 +0100

    Drycoded tabHash fixes for T6
    Will need testing if they fix it appropriately but known to be a problem.

--- evolve_automation.user.js
+++ evolve_automation.user.js
@@ -15022,7 +15022,10 @@ declare global {
           + (game.global.tech.alien_data >= 6 ? 1 : 0) // Alien Space Station decrypted (only required in lone survivor)
           + (game.global.tauceti.mining_ship?.count > 0 ? 1 : 0) // Extractor ship built
           + (game.global.tech.psychicthrall ?? 0) // Psychic powers
-          + (game.global.tech.psychic ?? 0); // Psychic powers
+          + (game.global.tech.psychic ?? 0) // Psychic powers
+          + (game.global.tech.isle >= 3 ? 1 : 0) // Edenic north/south piers -> spirit syphon tech
+          + (game.global.tech.palace >= 4 ? 1 : 0) // Edenic sealed tomb -> energy drain tech
+        ;
 
         if (game.global.settings.showShipYard) { // TP Ship Yard
           state.tabHash += 1

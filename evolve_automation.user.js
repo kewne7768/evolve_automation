0aa6743a2b2d5de79218af48078c8f429910276a
Volch <volk_pk@mail.ru>
2024-12-21 13:30:32 +0300

    Change links to github

--- evolve_automation.user.js
+++ evolve_automation.user.js
@@ -3,8 +3,8 @@
 // @namespace    http://tampermonkey.net/
 // @version      3.3.1.138
 // @description  try to take over the world!
-// @downloadURL  https://gist.github.com/Vollch/b1a5eec305558a48b7f4575d317d7dd1/raw/evolve_automation.user.js
-// @updateURL    https://gist.github.com/Vollch/b1a5eec305558a48b7f4575d317d7dd1/raw/evolve_automation.meta.js
+// @downloadURL  https://github.com/Vollch/Evolve-Automation/raw/master/evolve_automation.user.js
+// @updateURL    https://github.com/Vollch/Evolve-Automation/raw/master/evolve_automation.meta.js
 // @author       Fafnir
 // @author       TMVictor
 // @author       Vollch
